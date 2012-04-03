83fa169353dfd3768b6d4b9f1fe258f5578849b4
Joe Fitzpatrick <jf@emisense.com>
2012-04-02 20:21:07 -0700

    Incremented version to 1.0.1, merged with latest Juce (OpenGL changes).

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -30,8 +30,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "NativeTest";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "1.0.1";
+    const int          versionNumber  = 0x10001;
 }
 
 #endif   // __APPHEADERFILE_AB3KXD__
